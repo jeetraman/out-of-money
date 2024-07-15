ebc86277757662bed64642bd7974c1fd5b8ad9a7
RamanJeetKumar <ramanjeetkumar29100@gmail.com>
2024-07-15 13:58:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className='text-blue-600'>
+    <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      hii
+    </div>
+  )
+}
+
+export default Navbar
